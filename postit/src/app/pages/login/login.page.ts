03316fd135887de5b6d3febbfc255ab56421ec3d
giovanni3628 <giovannibortolosi@gmail.com>
2022-07-17 09:14:52 -0300

    Validações

--- postit/src/app/pages/login/login.page.ts
+++ postit/src/app/pages/login/login.page.ts
@@ -15,8 +15,24 @@ export class LoginPage implements OnInit {
     email: '',
     password: '',
   }
+
+  public isLoading: boolean = false;
+
   public login(): void {
+    this.isLoading = true;
     console.log(this.loginPayload);
+  }
+
+  public canLogin(): boolean {
+    const regex = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$');
+
+    const emailISValid = regex.test(this.loginPayload.email)
+
+    if(this.loginPayload.email.length >= 6&& this.loginPayload.password.length >= 6)
+      return true;
+
+    return false;
+
   }
 
   ngOnInit() {
